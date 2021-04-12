4a33c247b14be1ef878a7c2ef81ff3fd69366ffa
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-12 13:47:27 +0800

    Finished Input

--- App.js
+++ App.js
@@ -1,111 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Input} from './src/input';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Registration</Text>
+
+      <Input title="Name" placeholder="Masukan nama lengkap anda" />
+      <Input title="Username" placeholder="Masukan usename anda" />
+      <Input title="Email" placeholder="Masukan email anda" />
+      <Input title="Address" placeholder="Masukan alamat anda" />
+      <Input title="Phone Number" placeholder="Masukan nomor telepon anda" />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    padding: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    fontSize: 50,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  button: {
+    backgroundColor: '#C238CE',
+    height: 40,
+    borderRadius: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 10,
+    fontWeight: 'bold',
   },
 });
 
---
+++ src/input/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {TextInput, Text, View} from 'react-native';
+
+function Input(props) {
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 14,
+          fontWeight: '500',
+          marginTop: 20,
+          marginBottom: 8,
+        }}>
+        {props.title}
+      </Text>
+
+      <TextInput
+        placeholder={props.placeholder}
+        style={{
+          borderWidth: 1,
+          borderRadius: 10,
+          height: 37,
+          borderColor: '#C5C5C5',
+          padding: 10,
+        }}
+      />
+    </View>
+  );
+}
+
+export {Input};
