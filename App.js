0a15bdb77827833cc574b7ef7cbf4614743480ad
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-12 15:36:34 +0800

    Adding react-router-native

--- App.js
+++ App.js
@@ -1,49 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import {ScrollView, View, Text, StyleSheet} from 'react-native';
 
-import axios from 'axios';
+import {NativeRouter, Route, Link} from 'react-router-native';
 
 import Register from './Register';
-import Card from './src/card';
+import UserList from './UserList';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
-      setData(response.data);
-    });
-  }, []);
-
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>User List</Text>
-
-      {data.map(i => {
-        return (
-          <Card
-            key={i.id}
-            name={i.name}
-            username={i.username}
-            email={i.email}
-            address={`${i.address.street}, ${i.address.suite},${i.address.city}, ${i.address.zipcode} `}
-            phone={i.phone}
-          />
-        );
-      })}
-    </ScrollView>
+    <NativeRouter>
+      <View>
+        <Route path="/" component={Register} />
+        <Route path="/user-list" component={UserList} />
+      </View>
+    </NativeRouter>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 36,
-    fontWeight: 'bold',
-  },
-});
 
 export default App;
---
+++ UserList.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import {ScrollView, Text, StyleSheet} from 'react-native';
+
+import axios from 'axios';
+
+import Card from './src/card';
+
+function UserList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+      setData(response.data);
+    });
+  }, []);
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>User List</Text>
+
+      {data.map(i => {
+        return (
+          <Card
+            key={i.id}
+            name={i.name}
+            username={i.username}
+            email={i.email}
+            address={`${i.address.street}, ${i.address.suite},${i.address.city}, ${i.address.zipcode} `}
+            phone={i.phone}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+});
+
+export default UserList;
