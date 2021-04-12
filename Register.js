bebd0eb39b282607f138af61fda23b9fc8b9746d
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-12 15:00:19 +0800

    Adding Card

---
+++ Register.js
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import {ScrollView, Text, StyleSheet} from 'react-native';
+
+import {Input, NumericInput} from './src/input';
+import {RegisterButton} from './src/button';
+
+function Register() {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleSubmit = () => {
+    console.log(`Name: ${name}`);
+    console.log(`Username: ${username}`);
+    console.log(`Email: ${email}`);
+    console.log(`Address: ${address}`);
+    console.log(`Phone Number: ${phoneNumber}`);
+  };
+
+  const handleName = e => {
+    setName(e);
+  };
+
+  const handleUsrename = e => {
+    setUsername(e);
+  };
+
+  const handleEmail = e => {
+    setEmail(e);
+  };
+
+  const handleAddress = e => {
+    setAddress(e);
+  };
+
+  const handlePhoneNumber = e => {
+    setPhoneNumber(e);
+  };
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+      <Text style={styles.title}>Registration</Text>
+
+      <Input
+        title="Name"
+        placeholder="Masukan nama lengkap anda"
+        handleChange={handleName}
+      />
+      <Input
+        title="Username"
+        placeholder="Masukan usename anda"
+        handleChange={handleUsrename}
+      />
+      <Input
+        title="Email"
+        placeholder="Masukan email anda"
+        handleChange={handleEmail}
+      />
+      <Input
+        title="Address"
+        placeholder="Masukan alamat anda"
+        handleChange={handleAddress}
+      />
+      <NumericInput
+        title="Phone Number"
+        placeholder="Masukan nomor telepon anda"
+        handleChange={handlePhoneNumber}
+      />
+
+      <RegisterButton handleSubmit={handleSubmit} />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+  title: {
+    fontSize: 50,
+  },
+
+  button: {
+    backgroundColor: '#C238CE',
+    height: 40,
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+});
+
+export default Register;
---
+++ src/card/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {ScrollView, Text, StyleSheet, View} from 'react-native';
+
+function Card(props) {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.bold}>
+        Name: <Text style={styles.normal}>{props.name}</Text>
+      </Text>
+
+      <Text style={styles.bold}>
+        Username: <Text style={styles.normal}>{props.username}</Text>
+      </Text>
+
+      <Text style={styles.bold}>
+        Email: <Text style={styles.normal}>{props.email}</Text>
+      </Text>
+
+      <Text style={styles.bold}>
+        Address: <Text style={styles.normal}>{props.address}</Text>
+      </Text>
+
+      <Text style={styles.bold}>
+        Phone: <Text style={styles.normal}>{props.phone}</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderColor: 'grey',
+    borderRadius: 10,
+    borderWidth: 1,
+    paddingHorizontal: 26,
+    paddingVertical: 13,
+    marginBottom: 10,
+  },
+  bold: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  normal: {fontWeight: '100', fontSize: 14},
+});
+
+export default Card;
